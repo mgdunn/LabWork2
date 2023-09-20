8145efea646b46595e3fa4de4d4bb0d55632d06b
mgdunn <139691015+mgdunn@users.noreply.github.com>
2023-09-21 03:05:01 +1000

    Add files via upload

--- main.c
+++ main.c
@@ -3,6 +3,20 @@
 #include "graph.h"
 
 
+
+// Function to create a new graph
+Graph new_graph(int V) {
+    Graph graph;
+    graph.V = V;
+    graph.edges = (EdgeList*)malloc(V * sizeof(EdgeList));
+
+    for (int i = 0; i < V; i++) {
+        graph.edges[i].head = NULL;
+    }
+    return graph;
+}
+
+
 // Function to create a new edge node
 EdgeNodePtr createEdgeNode(int to_vertex, int weight) {
     EdgeNodePtr newNode = (EdgeNodePtr)malloc(sizeof(struct edgeNode));
@@ -13,43 +27,35 @@ EdgeNodePtr createEdgeNode(int to_vertex, int weight) {
     return newNode;
 }
 
+
 // Function to insert a node at the front of the list
 void insertAtFront(EdgeList* list, EdgeNodePtr newNode) {
     newNode->next = list->head;
     list->head = newNode;
 }
 
-// Function to create a new graph
-Graph new_graph(int V) {
-    Graph graph;
-    graph.V = V;
-    graph.edges = (EdgeList*)malloc(V * sizeof(EdgeList));
-  
-    for (int i = 0; i < V; i++) {
-        graph.edges[i].head = NULL;
-    }
-    return graph;
-}
-
 
 int main() {
     Graph G;
 
-    // Open the input file for reading
+    // Open the input file
     FILE* input_file = fopen("input.txt", "r");
 
     scanf_s("%d", &G.V);
 
-    // Initialize an array of in-degrees and set them all to 0
-    int* in_degrees = (int*)malloc(G.V * sizeof(int));
+
+    // Initialize an array of edge counts and set them all to 0
+    int* edgeCounts = (int*)malloc(G.V * sizeof(int));
     for (int i = 0; i < G.V; i++) {
-        in_degrees[i] = 0;
+        edgeCounts[i] = 0;
     }
 
-    // Inner for loop that loops through each edge
+
+    // Loops through each vertex
     for (int i = 0; i < G.V; i++) {
         int numEdges;
         scanf_s("%d", &numEdges); 
+
 
         // Loop through each edge for the current vertex
         for (int j = 0; j < numEdges; j++) {
@@ -62,16 +68,18 @@ int main() {
             EdgeNodePtr newNode = createEdgeNode(to_vertex, weight);
             insertAtFront(&(G.edges[i]), newNode);
 
-            // Increment the in-degree for the to-vertex
-            in_degrees[to_vertex]++;
+            // Increment the edge count for the to-vertex
+            edgeCounts[to_vertex] = edgeCounts[to_vertex] + weight;
         }
     }
+
 
     // Print the in-degrees
     printf("In-degrees:\n");
     for (int i = 0; i < G.V; i++) {
-        printf("Vertex %d: %d\n", i, in_degrees[i]);
+        printf("Vertex %d: %d\n", i, edgeCounts[i]);
     }
+
 
     // Free allocated memory
     for (int i = 0; i < G.V; i++) {
